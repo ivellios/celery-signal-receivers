a34d95a428bbaedcdf151d2d31e5334d43dcd178
Janusz Kamienski <januszk@wix.com>
2023-09-21 16:45:24 +0200

    adding initial code files

---
+++ tests/testapp/celery.py
@@ -0,0 +1,26 @@
+import logging
+import os
+
+from celery import Celery
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+
+app = Celery("project")
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Load task modules from all registered Django apps.
+app.autodiscover_tasks()
+
+
+logger = logging.getLogger(__file__)
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    logger.info("PING")
